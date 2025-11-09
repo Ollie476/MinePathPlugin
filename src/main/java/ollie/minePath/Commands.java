ce7880a9ab5c8341af35160a94afdbbdef9b1f0a
Ollie476 <olibobadventuresyt@gmail.com>
2025-11-09 22:50:36 +0000

    second iteration

--- src/main/java/ollie/minePath/Commands.java
+++ src/main/java/ollie/minePath/Commands.java
@@ -20,44 +20,36 @@ public class Commands implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (sender instanceof Player commandSender) {
             switch (args[0]) {
-                case "set":
-                    setDelay(commandSender, args[1]);
+                case "set_delay":
+                    plugin.setDelay(commandSender, args[1]);
                     break;
                 case "start":
                     try {
                         plugin.startSnapshotTask();
                     } catch (IOException ignored) {}
-                    sender.sendMessage(ChatColor.BOLD+ "" + ChatColor.GOLD + "[MINEPATH] Successfully started the recorder");
                     break;
                 case "stop":
                     try {
                         plugin.stopSnapshotTask();
                     } catch (IOException ignored) {}
-                    sender.sendMessage(ChatColor.BOLD+ "" + ChatColor.GOLD + "[MINEPATH] Successfully stopped the recorder");
                     break;
-                case "get":
-                    sender.sendMessage(ChatColor.BOLD+ "" + ChatColor.GOLD + "The recorder records in intervals of " + plugin.snapshotDelay + " seconds");
+                case "info":
+                    sender.sendMessage("--------------------");
+                    sender.sendMessage(ChatColor.BOLD+ "" + ChatColor.GOLD + "[MINEPATH] Recording intervals: " + plugin.snapshotDelay + " second(s)");
+                    sender.sendMessage(ChatColor.BOLD+ "" + ChatColor.GOLD + "[MINEPATH] Current snapshot index: " + plugin.snapshotIndex);
+                    if (plugin.getConfig().getBoolean("is_playing"))
+                        sender.sendMessage(ChatColor.BOLD+ "" + ChatColor.GOLD + "[MINEPATH] The recorder is on");
+                    else
+                        sender.sendMessage(ChatColor.BOLD+ "" + ChatColor.GOLD + "[MINEPATH] The recorder is off");
+                    sender.sendMessage("--------------------");
                     break;
-                case "clear":
+                case "reset":
                     try {
                         plugin.clearPlayerDataFile();
                     } catch (IOException ignored) {}
-                    sender.sendMessage(ChatColor.BOLD+ "" + ChatColor.GOLD + "The player_data.csv file has been cleared");
                     break;
-
             }
         }
         return true;
-    }
-
-    private void setDelay(Player sender, String newDelay) {
-        try {
-            plugin.snapshotDelay = Integer.parseInt(newDelay);;
-            plugin.startSnapshotTask();
-            sender.sendMessage(ChatColor.BOLD+ "" + ChatColor.GOLD + "[MINEPATH] Snapshot delay is now " + newDelay + " seconds");
-        }
-        catch (Exception e) {
-            sender.sendMessage(ChatColor.BOLD+ "" + ChatColor.RED + "[MINEPATH] " + newDelay + " is not an integer value");
-        }
     }
 }
--- src/main/java/ollie/minePath/MinePath.java
+++ src/main/java/ollie/minePath/MinePath.java
@@ -1,6 +1,8 @@
 package ollie.minePath;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
@@ -9,27 +11,47 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public final class MinePath extends JavaPlugin {
-    public long snapshotDelay = 30;
-    public long snapshotIndex = 0;
+    public long snapshotDelay;
+    public long snapshotIndex;
 
     private final String filePath = "player_data.csv";
+    private final List<Location> playerLocations = new ArrayList<>();
 
     private static BukkitTask snapshotTask;
 
     @Override
     public void onEnable() {
+        saveDefaultConfig();
+
+        if (!getConfig().contains("delay"))
+            getConfig().set("delay", 30);
+        if (!getConfig().contains("snapshot_index"))
+            getConfig().set("snapshot_index", 0);
+        if (!getConfig().contains("is_playing"))
+            getConfig().set("is_playing", false);
+
+        saveConfig();
+
+        snapshotDelay = getConfig().getInt("delay");
+        snapshotIndex = getConfig().getInt("snapshot_index");
+        boolean is_playing = getConfig().getBoolean("is_playing");
+
+        if (is_playing) {
+            try {
+                startSnapshotTask();
+            } catch (IOException ignored) {}
+        }
+
         getCommand("minepath").setExecutor(new Commands(this));
         getCommand("minepath").setTabCompleter(new TabCompleter() {
             @Override
             public List<String> onTabComplete(CommandSender commandSender, Command command, String s, String[] args) {
-                String[] results = {"start", "stop", "get", "set", "clear"};
+                String[] results = {"start", "stop", "info", "set_delay", "reset"};
                 List<String> out = new ArrayList<>();
 
                 if (args.length == 1) {
@@ -38,7 +60,6 @@ public final class MinePath extends JavaPlugin {
                             out.add(result);
                     return out;
                 }
-
                 return List.of();
             }
         });
@@ -49,6 +70,10 @@ public final class MinePath extends JavaPlugin {
             snapshotTask.cancel();
         }
 
+        getConfig().set("is_playing", true);
+        saveConfig();
+        sendToOperators(ChatColor.BOLD+ "" + ChatColor.GOLD + "[MINEPATH] Successfully started the recorder");
+
         snapshotTask = new BukkitRunnable() {
             @Override
             public void run() {
@@ -56,12 +81,15 @@ public final class MinePath extends JavaPlugin {
 
                 for (Player player : Bukkit.getOnlinePlayers()) {
                     fileAdditions.append(snapshotIndex).append(",").append(player.getName()).append(",").append(player.getLocation().getBlockX()).append(",").append(player.getLocation().getBlockZ()).append("\n");
+                    playerLocations.add(player.getLocation());
                 }
 
                 try {
                     addToPlayerDataFile(fileAdditions.toString());
                 } catch (IOException ignored) {}
                 snapshotIndex++;
+                getConfig().set("snapshot_index", snapshotIndex);
+                saveConfig();
             }
 
             public void addToPlayerDataFile(String data) throws IOException {
@@ -87,6 +115,23 @@ public final class MinePath extends JavaPlugin {
     public void stopSnapshotTask() throws IOException {
         if (snapshotTask != null) {
             snapshotTask.cancel();
+            getConfig().set("is_playing", false);
+            saveConfig();
+
+            double minX = Double.MAX_VALUE, minZ = Double.MAX_VALUE;
+            double maxX = Double.MIN_VALUE, maxZ = Double.MIN_VALUE;
+
+            for (Location loc : playerLocations) {
+                if (loc.getBlockX() < minX) minX = loc.getBlockX();
+                if (loc.getBlockZ() < minZ) minZ = loc.getBlockZ();
+                if (loc.getBlockX() > maxX) maxX = loc.getBlockX();
+                if (loc.getBlockZ() > maxZ) maxZ = loc.getBlockZ();
+            }
+            sendToOperators(ChatColor.BOLD + "" + ChatColor.GOLD + "[MINEPATH] Successfully stopped the recorder");
+            sendToOperators("--------------------");
+            sendToOperators(ChatColor.BOLD + "" + ChatColor.GOLD + "[MINEPATH] TopLeft: (" + minX + ", " + minZ + ")");
+            sendToOperators(ChatColor.BOLD + "" + ChatColor.GOLD + "[MINEPATH] BottomRight: (" + maxX + ", " + maxZ + ")");
+            sendToOperators("--------------------");
         }
     }
 
@@ -105,6 +150,33 @@ public final class MinePath extends JavaPlugin {
         FileWriter writer = new FileWriter(file, false);
         writer.write("");
         writer.close();
+
+        sendToOperators(ChatColor.BOLD+ "" + ChatColor.GOLD + "[MINEPATH] The recording data has been cleared");
+
+        playerLocations.clear();
         snapshotIndex = 0;
+        getConfig().set("snapshot_index", 0);
+        getConfig().set("is_playing", false);
+        stopSnapshotTask();
+        saveConfig();
+    }
+
+    public void setDelay(Player sender, String newDelay) {
+        try {
+            snapshotDelay = Integer.parseInt(newDelay);
+            getConfig().set("delay", snapshotDelay);
+            saveConfig();
+            sendToOperators(ChatColor.BOLD+ "" + ChatColor.GOLD + "[MINEPATH] Snapshot delay is now " + newDelay + " seconds");
+        }
+        catch (Exception e) {
+            sender.sendMessage(ChatColor.BOLD+ "" + ChatColor.RED + "[MINEPATH] " + newDelay + " is not an integer value");
+        }
+    }
+
+    private void sendToOperators(String message) {
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            if (player.isOp())
+                player.sendMessage(message);
+        }
     }
 }
